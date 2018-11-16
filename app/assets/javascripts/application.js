a5b1f8b04d9599be7068469ecaa2218c239a6d62
jdsewell <jdsewell@protonmail.com>
2018-11-16 12:08:20 -0500

    Edited views and fixed loofah

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,12 +21,12 @@
 
 $(document).on('turbolinks:load', function() {
   $('.unfollow_btn').hover(function(){
-    $(this).removeClass('btn-primary');
+    $(this).removeClass('btn-follow');
     $(this).addClass('btn-danger');
     $(this).html("Unfollow");
   }, function(){
     $(this).html("Following");
     $(this).removeClass('btn-danger');
-    $(this).addClass('btn-primary');
+    $(this).addClass('btn-follow');
   });
 })
