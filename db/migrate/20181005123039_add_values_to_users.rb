a6e1c948d70ae0d2fda1fb40277171ab1ae8bc83
jdsewell <jdsewell@protonmail.com>
2018-12-03 12:15:25 -0500

    Fixed db migration issues

--- db/migrate/20181005123039_add_values_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddValuesToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :username, :string
-    add_column :users, :bio, :text
-    add_column :users, :location, :string
-  end
-end
